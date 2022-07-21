09c9c4f03cc03ac06f45e651d77a6f4c2f2e0eaf
Vivek Maskara <maskaravivek@gmail.com>
2022-07-21 19:30:36 -0400

    Update code with snippets in the tutorial

--- lib/aws-cdk-api-auth-lambda-circle-ci-stack.ts
+++ lib/aws-cdk-api-auth-lambda-circle-ci-stack.ts
@@ -1,16 +1,86 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import {
+  Stack,
+  StackProps,
+  aws_s3 as s3,
+  aws_dynamodb as dynamodb,
+  aws_lambda as lambda,
+  aws_apigateway as apigateway,
+  Duration
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCdkApiAuthLambdaCircleCiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // we will add all the constructs here
+    const circleCiGwpBucket = new s3.Bucket(this, "circle-ci-gwp-bucket", {
+      bucketName: "circle-ci-auth-example-gwp-bucket",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCdkApiAuthLambdaCircleCiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const circleCiGwpTable = new dynamodb.Table(this, "CircleCIGwpTable", {
+      tableName: "CircleCIGwpAuthExampleTable",
+      partitionKey: { name: "jobId", type: dynamodb.AttributeType.STRING },
+    });
+
+    const circleCiGwpLambda = new lambda.Function(
+      this,
+      "CircleCiGwpProcessJobLambda",
+      {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "index.handler",
+        timeout: Duration.seconds(30),
+        code: lambda.Code.fromAsset("lambda/processJob/"),
+        environment: {
+          TABLE_NAME: circleCiGwpTable.tableName,
+          BUCKET_NAME: circleCiGwpBucket.bucketName
+        },
+      }
+    );
+
+    circleCiGwpBucket.grantPut(circleCiGwpLambda);
+    circleCiGwpTable.grantReadWriteData(circleCiGwpLambda);
+
+    const circleCiAuthLambda = new lambda.Function(
+      this,
+      "CircleCiAuthLambda",
+      {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: "index.handler",
+        timeout: Duration.seconds(30),
+        code: lambda.Code.fromAsset("lambda/authorizer/"),
+      }
+    );
+
+    const circleCiAuthorizer = new apigateway.TokenAuthorizer(this, 'CircleCIGWPAuthorizer', {
+      handler: circleCiAuthLambda
+    });
+
+    const circleCiGwpApi = new apigateway.RestApi(this, "CircleCIGWPAPI", {
+      restApiName: "Circle CI GWP API",
+      description: "Sample API for Circle CI GWP"
+    });
+
+    const jobResource = circleCiGwpApi.root.addResource("jobs");
+
+    const processJobIntegration = new apigateway.LambdaIntegration(
+      circleCiGwpLambda
+    );
+
+    jobResource.addMethod("POST", processJobIntegration, {
+      authorizer: circleCiAuthorizer,
+      authorizationType: apigateway.AuthorizationType.CUSTOM,
+    });
+
+    const circleCiUsagePlan = circleCiGwpApi.addUsagePlan('CircleCiUsagePlan', {
+      name: 'CircleCiEasyPlan',
+      throttle: {
+        rateLimit: 100,
+        burstLimit: 2
+      }
+    });
+
+    const circleCiApiKey = circleCiGwpApi.addApiKey('CircleCiApiKey');
+    circleCiUsagePlan.addApiKey(circleCiApiKey);
   }
 }
